aa2c6fef1ec3c29b182bc9503a671ea19ee7a370
Gonçalo Penelas <goncalopenelas2000@hotmail.com>
2026-04-01 17:50:05 +0100

    Project reorganized and sign collision corrected

--- src/collision.js
+++ src/collision.js
@@ -1,7 +1,5 @@
 objetosColisao = [];
 
-
-
 function distancia(p1, p2) {
   var dx = p1.x - p2.x;
   var dy = p1.y - p2.y;
@@ -10,16 +8,15 @@ function distancia(p1, p2) {
   return Math.sqrt(dx * dx + dy * dy + dz * dz);
 }
 
-function detetarColisao()
-{
+function detetarColisao() {
   var RaioCarro = 10;
 
-  arrayMoedas.forEach(Moeda => {
+  arrayMoedas.forEach((Moeda) => {
     var distanciaMoedaCarro = distancia(Moeda.position, carro.position);
     // console.log(distanciaMoedaCarro);
     var RaioMoeda = 10;
 
-    if (distanciaMoedaCarro < RaioCarro + RaioMoeda ) {
+    if (distanciaMoedaCarro < RaioCarro + RaioMoeda) {
       jogo.pontuacao += jogo.valorMoeda;
       NovaMoeda(Moeda);
     }
@@ -28,14 +25,14 @@ function detetarColisao()
   // if(StopSinal && carro)
   // {
   //   var matrix = new THREE.Matrix4();
-	// 	matrix.extractRotation(StopSinal.matrix);
-	// 	var directionFront = new THREE.Vector3(0,0,1);
-	// 	directionFront.applyMatrix4(matrix);
-	// 	var rayCaster = new THREE.Raycaster(StopSinal.position,directionFront);
-	// 	if(rayIntersect(rayCaster,0.1))
-	// 	{
-	// 		jogo.status = "gameover";
-	// 	}
+  // 	matrix.extractRotation(StopSinal.matrix);
+  // 	var directionFront = new THREE.Vector3(0,0,1);
+  // 	directionFront.applyMatrix4(matrix);
+  // 	var rayCaster = new THREE.Raycaster(StopSinal.position,directionFront);
+  // 	if(rayIntersect(rayCaster,0.1))
+  // 	{
+  // 		jogo.status = "gameover";
+  // 	}
 
   //   function rayIntersect(ray, distance) {
   //     var intersects = ray.intersectObjects(objetosColisao);
@@ -48,25 +45,25 @@ function detetarColisao()
   //   }
   // }
 
-  arrayParede.forEach(parede => {
-     var distanciaSinalCarro = distancia(parede.position, carro.position);
-     console.log(distanciaSinalCarro);
-     var raioSinal = 13; 
-     if (distanciaSinalCarro < RaioCarro + raioSinal) {
-       jogo.status = "gameover";
-    }
- });
+  arrayParede.forEach((parede) => {
+    var posicaoMundial = new THREE.Vector3();
+    parede.getWorldPosition(posicaoMundial);
 
-  arrayNuvens.forEach(nuvem => {
-    var distanciaNuvemCarro = distancia(nuvem.position, carro.position);
-
-    var RaioNuvem = 10;
-
-    if (distanciaNuvemCarro < RaioCarro + RaioNuvem ) {
+    var distanciaSinalCarro = distancia(posicaoMundial, carro.position);
+    // console.log("Distancia parede-carro:", distanciaSinalCarro);
+    var raioSinal = 50;
+    if (distanciaSinalCarro < RaioCarro + raioSinal) {
       jogo.status = "gameover";
     }
   });
 
+  arrayNuvens.forEach((nuvem) => {
+    var distanciaNuvemCarro = distancia(nuvem.position, carro.position);
 
+    var RaioNuvem = 10;
 
+    if (distanciaNuvemCarro < RaioCarro + RaioNuvem) {
+      jogo.status = "gameover";
+    }
+  });
 }
